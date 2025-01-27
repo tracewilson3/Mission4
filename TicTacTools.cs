6f820c8f2b999a94c39192fbd143c7d7e0c9fd79
grantbear <153117224+grantbear@users.noreply.github.com>
2025-01-27 13:22:56 -0700

    Check Winner Method

--- TicTacTools.cs
+++ TicTacTools.cs
@@ -41,5 +41,43 @@ public class TicTacTools
                 }
             }
         }
+        public static string CheckWinner(string[,] board)
+    {
+        int rows = board.GetLength(0);
+        int cols = board.GetLength(1);
+
+        // Check rows for a winner
+        for (int i = 0; i < rows; i++)
+        {
+            if (board[i, 0] != "_" && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+            {
+                return board[i, 0]; // Return the winner (X or O)
+            }
+        }
+
+        // Check columns for a winner
+        for (int j = 0; j < cols; j++)
+        {
+            if (board[0, j] != "_" && board[0, j] == board[1, j] && board[1, j] == board[2, j])
+            {
+                return board[0, j];
+            }
+        }
+
+        // Check diagonals for a winner
+        if (board[0, 0] != "_" && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+        {
+            return board[0, 0];
+        }
+
+        if (board[0, 2] != "_" && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+        {
+            return board[0, 2];
+        }
+
+        // If no winner, return "None"
+        return "None";
+    }
+}
     }
 
