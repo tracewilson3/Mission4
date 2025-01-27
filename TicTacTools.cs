57efbb406d1906a2389f5f9e988213da0f24c49b
Grant.Bear <gbeardal@byu.edu>
2025-01-27 12:42:38 -0700

    Tic Tac Toe Board

--- TicTacTools.cs
+++ TicTacTools.cs
@@ -2,5 +2,16 @@ namespace Mission4;
 
 internal class TicTacTools
 {
-    
+    string[,] board =
+    {
+        { "_", "_", "_" },
+        { "_", "_", "_" },
+        { "_", "_", "_" }
+    };
+
+
+
+
+
+
 }
